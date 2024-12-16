d103cd1cfbe076eb6c986740100a99a0155c897b
daFoggo <ntgiang141105@gmail.com>
2024-12-16 16:21:37 +0700

    modify available slot work fine ? right ? RIGHT?

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
+import Logo from "@/components/common/Logo";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex items-center justify-center h-[calc(100vh-300px)]">
+      <div className="flex space-x-6 items-center justify-center">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={0}
+          height={0}
+          style={{ width: 100, height: 100 }}
           priority
         />
-      </main>
+        <p>X</p>
+        <Logo />
+      </div>
     </div>
   );
 }

ab3b5a53e276f9e68f57556c624f2061784991e8
daFoggo <ntgiang141105@gmail.com>
2025-01-06 15:45:03 +0700

    update logo

--- components/common/Logo/index.tsx
+++ components/common/Logo/index.tsx
@@ -1,10 +1,7 @@
 const Logo = () => {
   return (
     <p className="sm:inline-block text-2xl font-clash font-semibold">
-      <span className="text-primary">1</span>
-      MIN
-      <span className="text-primary">2</span>
-      MEET
+      SCHEDOU
     </p>
   );
 };
